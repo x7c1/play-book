3df9e8795eab21deb841881a6ab0b76f16826caf
x7c1 <jmtaro+x7c1@gmail.com>
2018-05-04 17:50:52 +0900

    rename

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import { Chapter, main } from "book-assert";
 
 main({
   docsDir: "./docs",
-  outDir: "./docs-gen",
+  outDir: "./ts.book-assert.gen",
   loader: id => require(id).chapter as Chapter,
   argv: process.argv,
 });
