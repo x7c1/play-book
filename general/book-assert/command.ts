f3d98acccae7dcd326ff59218ed30761b65059ad
x7c1 <jmtaro+x7c1@gmail.com>
2018-05-07 02:13:02 +0900

    update descriptions

--- general/book-assert/command.ts
+++ general/book-assert/command.ts
@@ -3,11 +3,11 @@ import { Command } from "commander";
 export const createCommand: (argv: string[]) => Command = argv =>
   new Command()
     .version("0.0.1")
-    .option("--serve", "serve a gitbook markdown files")
-    .option("--chapter <number>", "chapter to assert", n => {
+    .option("--serve", "Build then serve the gitbook files")
+    .option("--chapter <number>", "Chapter to assert", n => {
       const i = parseInt(n, 10);
       return isNaN(i) ? "" : i.toString().padStart(3, "0");
     })
-    .option("--section <title>", "section to assert")
-    .option("--single", "assert only given chapter (or its section)")
+    .option("--section <title>", "Section to assert")
+    .option("--single", "Assert only given chapter (or its section)")
     .parse(argv);
