f7517fa0eb2bb6c4772d52282983e7128be1f879
x7c1 <jmtaro+x7c1@gmail.com>
2018-05-09 00:41:18 +0900

    add readme field

--- general/book-assert/Book.ts
+++ general/book-assert/Book.ts
@@ -1,7 +1,18 @@
-import { serve } from "./serve";
+export interface Readme {
+  title: string;
+  path: string;
+}
 
 export class Book {
   serve(srcDir: string, dstDir: string): Promise<void> {
     return serve(srcDir, dstDir);
   }
+
+  get readme(): Readme {
+    return {
+      title: "readme title",
+      path: this.readmePath,
+    };
+  }
+
 }
