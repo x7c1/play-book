a9f8a058d8d597ec0ae526786ef5daae2adee545
x7c1 <jmtaro+x7c1@gmail.com>
2018-05-12 02:35:02 +0900

    add withParent.confirmFile

--- general/file-paths/DirectoryPath.ts
+++ general/file-paths/DirectoryPath.ts
@@ -1,13 +1,31 @@
 import { Confirm } from "./Confirm";
 import { HasAbsolutePath, PathResolver } from "./HasAbsolutePath";
 import { PathBase } from "./PathBase";
+import { confirm as confirmFile, FilePath } from "./FilePath";
+import { basename, dirname, join } from "path";
 
 export interface DirectoryPath extends HasAbsolutePath {
   toRelative: string;
+  withParent: WithParent;
   resolve(...segments: string[]): string;
 }
 
+export interface WithParent {
+  confirmFile(path: string): Promise<FilePath>;
+}
+
 class DirectoryPathImpl extends PathResolver(PathBase)
-  implements DirectoryPath {}
+  implements DirectoryPath {
+  get withParent(): WithParent {
+    return {
+      confirmFile: (path: string) => {
+        return confirmFile.fromRelative(
+          dirname(this.toAbsolute),
+          join(basename(this.toAbsolute), path),
+        );
+      },
+    };
+  }
+}
 
 export const confirm: Confirm<DirectoryPath> = Confirm(DirectoryPathImpl);
